0b69f32ec0e1ad3f315d8da2fb93c7b9f0f07188
AHMAD <ahmedabbous.m@gmail.com>
2023-05-26 12:09:09 +0400

    working on the theme

--- app/public/wp-content/themes/wordpresstheme/single.php
+++ app/public/wp-content/themes/wordpresstheme/single.php
@@ -18,12 +18,3 @@ get_header();
     </section>
 <?php
 get_footer();
-/*
-now the issue somehow fixed, by enabling the visual editor
-i can see in my page every paragraph wrapped with p tag, but also the subtitles also wrapped with p tag
-i want an automated way to wrap the subtitle with h2 tag
-
-
-in my page i have logo, i have post title, i have subtites
-what is the best tags to use for it in regards best practice seo
- */
